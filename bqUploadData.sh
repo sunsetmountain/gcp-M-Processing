2aa6d4ea7d289922bbfd69f1fde87beb3dbee1e3
sunsetmountain <brandon@sunsetmountain.com>
2021-07-28 14:52:23 -0600

    Update bqUploadData.sh

--- bqUploadData.sh
+++ bqUploadData.sh
@@ -129,6 +129,6 @@ bq mk \
     PI._Cost_Month__Pay_As_You_Go__No_Contract_ as AzureVMCost
 from movereDataset.arc_landscape_device_summary DS
     inner join movereDataset.azure_vm_sizing SI on SI.Device_Name = DS.Device_Name
-    inner join movereDataset.arc_azure_vm_pricing PI on PI.Device_name = SI.Device_Name
+    inner join movereDataset.azure_vm_pricing PI on PI.Device_name = SI.Device_Name
 Order by AzureVMCost Desc' \
 movereDataset.Azure_VM_Comparison
